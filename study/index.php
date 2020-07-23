9a882971071f0689b1b8e3b74d878af36c3f291d
skatn <55577382+skatn@users.noreply.github.com>
2020-07-24 00:06:32 +0900

    글쓰기 작업 진행중

--- study/index.php
+++ study/index.php
@@ -1,8 +1,48 @@
+<?php
+function print_title(){
+  if(isset($_GET['id'])){
+    echo $_GET['id'];
+  }
+  else {
+    echo 'Welcome';
+  }
+}
+
+function print_description(){
+  if(isset($_GET['id'])){
+    echo file_get_contents('data/'.$_GET['id']);
+  }
+}
+
+function print_list(){
+  $list = scandir('./data');
+
+  $i = 2;
+  while($i < count($list)){
+    echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>";
+    $i = $i + 1;
+  }
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+        <?php
+          print_title();
+        ?>
+      </title>
+  </head>
   <body>
-    <?php
-      echo date('Y-m-d H:i:s');
-    ?>
+    <h1><a href='index.php'>CRUD</a></h1>
+    <ol>
+      <?php
+        print_list();
+      ?>
+    </ol>
+    <a href="create.php"></a>
   </body>
 </html>
